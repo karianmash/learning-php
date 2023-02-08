08f1fc7ddb4cece2cd46e5b27d8a63b3fd45799e
karianmash <ianmachariak17@gmail.com>
2023-02-08 10:25:02 +0300

    Covered associative arrays

--- 5.arrays.php
+++ 5.arrays.php
@@ -59,3 +59,25 @@ foreach ($age as $x => $x_value) {
     echo "<br>";
 }
 ?>
+
+<!-- Looping through a multidimensional array -->
+<?php
+$cars = array(
+    array("Volvo", 22, 18),
+    array("BMW", 15, 13),
+    array("Saab", 5, 2),
+    array("Land Rover", 17, 15)
+);
+
+// Loop through the rows of the array
+for ($row = 0; $row < 4; $row++) {
+    echo "<p><b>Row number $row</b></p>";
+    echo "<ul>";
+
+    // Loop through the elements of the row
+    for ($col = 0; $col < 3; $col++) {
+        echo "<li>" . $cars[$row][$col] . "</li>";
+    }
+    echo "</ul>";
+}
+?>
