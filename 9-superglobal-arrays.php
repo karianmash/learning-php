d49694ce7d220f4c3a3f5a4264961f9c2ae5c47b
karianmash <ianmachariak17@gmail.com>
2023-02-15 13:52:56 +0300

    Looked at superglobal variables

--- 9-superglobal-arrays.php
+++ 9-superglobal-arrays.php
@@ -134,6 +134,24 @@ $_SESSION['name'] = 'John';
 echo $GLOBALS['name'];
 // The $GLOBALS["email"] accesses the global variable named "email"
 echo $GLOBALS['email'];
-// Define Global Variables
-$name = 'John';
+
+// Declaring global variable
+$x = "Geeks";
+$y = "for";
+$z = "Geeks";
+$a = 5;
+$b = 10;
+
+function concatenate()
+{
+    // Using global keyword
+    global $x, $y, $z;
+    return $x . $y . $z;
+}
+
+function add()
+{
+    // Using GLOBALS['var_name']
+    $GLOBALS['b'] = $GLOBALS['a'] + $GLOBALS['b'];
+}
 // -----------------------------------------------------------------------
