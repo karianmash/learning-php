8b6b2f9139d4cb62deee4e71cf3b20c316d121d9
karianmash <ianmachariak17@gmail.com>
2023-02-15 13:33:26 +0300

    Looked at superglobal variables

--- 9-superglobal-arrays.php
+++ 9-superglobal-arrays.php
@@ -1,12 +1,11 @@
 <!-- Superglobal variables in PHP -->
+<!-- Superglobal variables are built-in variables that are always available in all scopes. i.e, $GLOBALS -->
 <!-- ----------------------------------------------------------------------- -->
 
 <?php
-// Superglobal variables are built-in variables that are always available in all scopes
-// $GLOBALS
-
 // 1. $_SERVER
 // -----------------------------------------------------------------------
+
 // $_SERVER is a PHP super global variable which holds information about headers,
 //  paths, and script locations.
 // The example below shows how to use some of the elements in $_SERVER:
@@ -23,5 +22,22 @@ echo $_SERVER['HTTP_REFERER'];
 echo $_SERVER['HTTP_USER_AGENT'];
 // The $_SERVER["SCRIPT_NAME"] returns the path of the current script as a string
 echo $_SERVER['SCRIPT_NAME'];
-
 // -----------------------------------------------------------------------
+
+// 2. $_REQUEST
+// -----------------------------------------------------------------------
+
+// $_REQUEST is a PHP super global variable which is used to collect data after 
+// submitting an HTML form.
+// The example below shows how to use some of the elements in $_REQUEST:
+
+// The $_REQUEST["name"] returns the value of the name field from the form
+echo $_REQUEST['name'];
+// The $_REQUEST["email"] returns the value of the email field from the form
+echo $_REQUEST['email'];
+// -----------------------------------------------------------------------
+
+// 3. $_POST
+// -----------------------------------------------------------------------
+
+// $_POST is a PHP super global variable which is used to collect form data after
